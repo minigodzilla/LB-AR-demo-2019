dfdd4097dc81ab1cb74b3a6b8b954968f7557d1a
Steve Diabo <stevediabo@gmail.com>
2024-05-12 20:31:20 -0400

    customized launcher

--- Assets/Scripts/AR/ARLauncher.cs
+++ Assets/Scripts/AR/ARLauncher.cs
@@ -4,30 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class ARLauncher : MonoBehaviour
 {
-    [SerializeField] private Button DemoTestButton = default, ShowroomAButton = default,
-        ModelSwappingButton = default, TextureSwappingButton = default, MeshPrintingOneShotButton = default,
-        MeshPrintingStagedButton = default, ShowroomBButton = default;
+    [SerializeField]
+    private Button ShowroomAButton = default, ShowroomBButton = default, ShowroomCButton = default;
 
     [SerializeField] private Text buildText = default;
 
     private const string buildFormat = "BUILD VERSION: {0}";
-    private const string DemoScene = "DemoScene";
     private const string ShowroomA = "ShowroomA";
     private const string ShowroomB = "ShowroomB";
-    private const string ModelSwapping = "ModelSwapping";
-    private const string TextureSwapping = "TextureSwapping";
-    private const string MeshPrintingOneShot = "3DPrintingOneShot";
-    private const string MeshPrintingStaged = "3DPrintingStaged";
+    private const string ShowroomC = "ShowroomC";
 
     public void Awake()
     {
-        DemoTestButton.onClick.AddListener(() => LoadScene(DemoScene));
         ShowroomAButton.onClick.AddListener(() => LoadScene(ShowroomA));
         ShowroomBButton.onClick.AddListener(() => LoadScene(ShowroomB));
-        ModelSwappingButton.onClick.AddListener(() => LoadScene(ModelSwapping));
-        TextureSwappingButton.onClick.AddListener(() => LoadScene(TextureSwapping));
-        MeshPrintingStagedButton.onClick.AddListener(() => LoadScene(MeshPrintingStaged));
-        MeshPrintingOneShotButton.onClick.AddListener(() => LoadScene(MeshPrintingOneShot));
+        ShowroomCButton.onClick.AddListener(() => LoadScene(ShowroomC));
 
         buildText.text = string.Format(buildFormat, Application.version);
     }
